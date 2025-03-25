7576f24074bf243ed482347b4bd5d8a0043a86d5
aarushi2146 <106542499+aarushi2146@users.noreply.github.com>
2025-03-25 12:22:08 +0530

    Delete AddressBookApp-UC2 directory

--- AddressBookApp-UC2/AddressBookApp/src/main/java/com/example/AddressBookApp/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.AddressBookApp.repository;
-
-import com.example.AddressBookApp.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
